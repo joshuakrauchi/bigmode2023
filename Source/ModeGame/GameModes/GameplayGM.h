e601398550415b9db3932774f565f96d9cd3bad7
Joshua Krauchi <joshuakrauchi@gmail.com>
2023-11-28 08:38:43 -0800

    Added boilerplate project files: game mode, game state, player state, player controller. Also added in a couple basic interfaces for gameplay.

---
+++ Source/ModeGame/GameModes/GameplayGM.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameplayGM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODEGAME_API AGameplayGM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
