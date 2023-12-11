ce931aec1a93d467cedab73032de24725dc252ac
Joshua Krauchi <joshuakrauchi@gmail.com>
2023-12-11 02:15:49 -0800

    Green weapon update. Added sparks, bullet travel, and bullet hole systems. Undid test grapple change.

--- Source/ModeGame/Weapons/GreenWeapon.cpp
+++ Source/ModeGame/Weapons/GreenWeapon.cpp
@@ -4,6 +4,11 @@
 #include "GreenWeapon.h"
 #include "Math/UnrealMathUtility.h"
 #include "Characters/BaseCharacter.h"
+#include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraSystem.h"
+#include "NiagaraComponent.h"
 
 AGreenWeapon::AGreenWeapon()
 {
@@ -16,54 +21,128 @@ void AGreenWeapon::BeginPlay()
 	Super::BeginPlay();
 }
 
-void AGreenWeapon::Tick(float DeltaTime)
+void AGreenWeapon::Tick(float DeltaSeconds)
 {
-	Super::Tick(DeltaTime);
+	Super::Tick(DeltaSeconds);
+
+	CurrentShotCooldownTime = FMath::Max(0.0f, (CurrentShotCooldownTime - DeltaSeconds));
+
+	if (bIsHoldingFire)
+	{
+		TryBeginFire();
+	}
 }
 
 bool AGreenWeapon::TryBeginFire()
 {
+	bIsHoldingFire = true;
+
+	if (!CanFire()) { return false; }
+
+	TObjectPtr<UWorld> World = GetWorld();
+	if (!IsValid(World)) { return false; }
+	
 	TObjectPtr<ABaseCharacter> BaseCharacter = GetOwner<ABaseCharacter>();
 	if (!IsValid(BaseCharacter)) { return false; }
+
+	FVector TraceStart = BaseCharacter->GetProjectileStartLocation();
 
 	FVector ParentLocation = EquippedTransform->GetLocation();
 	FVector ParentForward = EquippedTransform->GetRotation().GetForwardVector();
 
-	UWorld* World = GetWorld();
+	FHitResult Trace{ };
+	FCollisionQueryParams CollisionParams{};
+	CollisionParams.AddIgnoredActor(GetOwner());
 
-	if (IsValid(World)) 
+	// Finds hits
+	for (int i = 0; i < MaxPellets; ++i)
 	{
-		FHitResult Trace { };
+		FVector RandomSpread{ FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians), FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians), FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians) };
+		FVector AdjustedForward = (ParentForward + RandomSpread) * MaxRange;
 
-		// Finds hits
-		for (int i = 0; i < MaxPellets; ++i)
+		World->LineTraceSingleByChannel(Trace, TraceStart, ParentLocation + AdjustedForward, ECollisionChannel::ECC_Visibility, CollisionParams);
+		if (!Trace.IsValidBlockingHit())
 		{
-			FVector RandomSpread { FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians), FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians), FMath::RandRange(-MaxSpreadRadians, MaxSpreadRadians) };
-			FVector AdjustedForward = (ParentForward + RandomSpread) * MaxRange;
-			FCollisionQueryParams CollisionParams {};
-			CollisionParams.AddIgnoredActor(GetOwner());
-
-			World->LineTraceSingleByChannel(Trace, ParentLocation, ParentLocation + AdjustedForward, ECollisionChannel::ECC_Visibility, CollisionParams);
-			ABaseCharacter* HitCharacter = Cast<ABaseCharacter>(Trace.GetActor());
-			if (HitCharacter != nullptr)
-			{
-				IDamageable::Execute_OnDamaged(HitCharacter, GetFalloffAdjustedDamage(Trace.Distance), BaseCharacter->CharacterColour);
-			}
-
-			// Visual debug component
-			DrawDebugLine(World, ParentLocation, ParentLocation + AdjustedForward, { 255, 0, 0 }, false, 3.0f, 0U, 0.2f);
+			SpawnBulletTrail(AdjustedForward);
+			continue;
 		}
+
+		SpawnBulletTrail(Trace.ImpactPoint);
+
+		TScriptInterface<IDamageable> Damageable = nullptr;
+		Damageable.SetInterface(Cast<IDamageable>(Trace.GetActor()));
+		if (Damageable != nullptr)
+		{
+			TryDamageDamageable(Damageable, Trace.Distance);
+		}
+		else
+		{
+			SpawnDecal(Trace.ImpactPoint, Trace.ImpactNormal, Trace.GetComponent());
+
+			if (IsValid(ImpactSparksSystem))
+			{
+				UNiagaraFunctionLibrary::SpawnSystemAtLocation(World, ImpactSparksSystem, Trace.ImpactPoint);
+			}
+		}
+
+		// Visual debug component
+		//DrawDebugLine(World, ParentLocation, ParentLocation + AdjustedForward, { 255, 0, 0 }, false, 3.0f, 0U, 0.2f);
 	}
+
+	PlayFiringMontage();
+	CurrentShotCooldownTime = BaseShotCooldownTime;
 
 	// Do trace stuff
 	ReceiveTestFire();
 
-	return Super::TryBeginFire() && true;
+	return Super::TryBeginFire();
 }
-
 
 bool AGreenWeapon::TryEndFire()
 {
-	return Super::TryEndFire() && true;
+	bIsHoldingFire = false;
+
+	return Super::TryEndFire();
 }
 
+bool AGreenWeapon::CanFire() const
+{
+	return (CurrentShotCooldownTime <= 0.0f);
+}
+
+void AGreenWeapon::SpawnDecal(FVector Location, FVector Normal, USceneComponent* AttachComponent)
+{
+	if (!IsValid(DecalMaterial)) { return; }
+	if (!IsValid(AttachComponent)) { return; }
+
+	TObjectPtr<UDecalComponent> Decal = UGameplayStatics::SpawnDecalAttached(
+		DecalMaterial,
+		FVector(DecalSize),
+		AttachComponent,
+		NAME_None,
+		Location,
+		Normal.ToOrientationRotator(),
+		EAttachLocation::KeepWorldPosition,
+		300.0f
+	);
+	
+	if (!IsValid(Decal)) { return; }
+
+	Decal->SetFadeScreenSize(0.0f);
+}
+
+void AGreenWeapon::SpawnBulletTrail(FVector BeamEnd)
+{
+	TObjectPtr<UWorld> World = GetWorld();
+	if (!IsValid(World)) { return; }
+
+	TObjectPtr<USkeletalMeshComponent> Mesh = GetVisibleMesh();
+	if (!IsValid(Mesh)) { return; }
+
+	FVector MuzzleLocation = Mesh->GetSocketLocation(FName("Muzzle"));
+
+	TObjectPtr<UNiagaraComponent> BulletTrail = UNiagaraFunctionLibrary::SpawnSystemAtLocation(World, BulletTrailSystem, MuzzleLocation);
+	if (!IsValid(BulletTrail)) { return; }
+
+	BulletTrail->SetVectorParameter(FName("Beam End"), BeamEnd);
+}
--- Source/ModeGame/Weapons/GreenWeapon.h
+++ Source/ModeGame/Weapons/GreenWeapon.h
@@ -6,6 +6,9 @@
 #include "BaseWeapon.h"
 #include "GreenWeapon.generated.h"
 
+class UMaterialInterface;
+class UNiagaraSystem;
+
 /**
  * 
  */
@@ -14,12 +17,35 @@ class MODEGAME_API AGreenWeapon : public ABaseWeapon
 {
 	GENERATED_BODY()
 
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		float BaseShotCooldownTime = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+		float CurrentShotCooldownTime = 0.0f;
+
 private:
 	UPROPERTY(EditAnywhere)
 		int MaxPellets = 8;
+
 	UPROPERTY(EditAnywhere)
 		float MaxSpreadRadians = 0.1f;
+
+	UPROPERTY(EditAnywhere)
+		float DecalSize = 0.0f;
 	
+	UPROPERTY(EditAnywhere)
+		TObjectPtr<UMaterialInterface> DecalMaterial = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		TObjectPtr<UNiagaraSystem> ImpactSparksSystem = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		TObjectPtr<UNiagaraSystem> BulletTrailSystem = nullptr;
+
+	UPROPERTY()
+		bool bIsHoldingFire = false;
+
 public:
 	AGreenWeapon();
 
@@ -29,7 +55,7 @@ protected:
 public:
 	// Begin IFireable Interface
 	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	virtual void Tick(float DeltaSeconds) override;
 
 	virtual bool TryBeginFire() override;
 
@@ -37,5 +63,19 @@ public:
 	// End IFireable Interface
 
 	UFUNCTION(BlueprintImplementableEvent)
+		void PlayFiringMontage();
+
+	UFUNCTION(BlueprintImplementableEvent)
 		bool ReceiveTestFire();
+
+	UFUNCTION(BlueprintCallable)
+		bool CanFire() const;
+
+private:
+	UFUNCTION(BlueprintCallable)
+		void SpawnDecal(FVector Location, FVector Normal, USceneComponent* AttachComponent);
+
+	UFUNCTION(BlueprintCallable)
+		void SpawnBulletTrail(FVector BeamEnd);
+
 };
